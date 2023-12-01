a1a5a519a81b97082a28ab398e689506a9a28c0b
Dariaeg <dariaeg04@gmail.com>
2023-12-01 16:08:58 +0500

    исправила ошибки

--- js/main.js
+++ js/main.js
@@ -58,17 +58,25 @@ const generateId1 = createRandomUniqueNumber(1, PHOTO_DESCRIPTION_COUNT);
 const generatePhoto = createRandomUniqueNumber(1, PHOTO_DESCRIPTION_COUNT);
 const generateId2 = createRandomUniqueNumber(1, ID_COUNT);
 const generateAvatar = getRandomInteger(1, AVATAR_COUNT);
+const str1 = 'img/avatar-';
+const str2 = String(generateAvatar());
+const str3 = '.svg';
+const avatar = str1 + str2 + str3;
+const str4 = 'photos/';
+const str5 = generatePhoto();
+const str6 = '.svg';
+const photo = str4 + str5 + str6;
 
 const createComment = () => ({
   id: generateId2,
-  avatar: 'img/avatar-' + generateAvatar() + '.svg',
+  avatar: avatar,
   message: getRandomArrayElement(MESSAGE),
   name: getRandomArrayElement(NAME)
 });
 
 const createDescription = () => ({
   id: generateId1(),
-  url: 'photos/' + generatePhoto() + '.svg',
+  url: photo,
   description: getRandomArrayElement(DESCRIPTION),
   likes: getRandomInteger(15, 200),
   comments: Array.from({length: getRandomInteger(0, 30)}, createComment)
