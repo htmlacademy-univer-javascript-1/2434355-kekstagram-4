9d3803388abfbb26c2ce87b71401ed0d7e955cfb
Dariaeg <dariaeg04@gmail.com>
2023-12-19 02:11:45 +0500

    popup

---
+++ js/popup.js
@@ -0,0 +1,22 @@
+import {createDescription} from './data.js';
+
+const pictureListElement = document.querySelector('.pictures  container');
+const similarPictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const descriptions = createDescription();
+
+const picturesListFragment = document.createDocumentFragment();
+
+descriptions.forEach(({id, url, description, likes, comments})
+=> {
+  const pictureElement = similarPictureTemplate.cloneNode(true);
+  pictureElement.src = url;
+  pictureElement.alt = description;
+  pictureElement.querySelector('.picture__likes.').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments;
+  picturesListFragment.appendChild(pictureElement);
+});
+
+pictureListElement.appendChild(picturesListFragment);
