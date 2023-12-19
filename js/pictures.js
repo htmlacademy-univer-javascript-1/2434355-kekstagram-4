b0993b08b55a18bc2d9963ac41bb6022e15827c4
Dariaeg <dariaeg04@gmail.com>
2023-12-19 22:10:43 +0500

    pictures

--- js/pictures.js
+++ js/pictures.js
@@ -1,22 +1,27 @@
-import {photoDescriptions} from './data.js';
-
-const pictureListElement = document.querySelector('.pictures');
-const similarPictureTemplate = document.querySelector('#picture')
+const picturesList = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
-const descriptionsOfPictures = photoDescriptions();
+const createPicture = ({url, description, likes, comments}) => {
+  const picture = pictureTemplate.cloneNode(true);
+  picture.querySelector('.picture__img.').src = url;
+  picture.querySelector('.picture__img.').alt = description;
+  picture.querySelector('.picture__likes.').textContent = likes;
+  picture.querySelector('.picture__comments').textContent = comments.length;
 
-const picturesListFragment = document.createDocumentFragment();
+  return picture;
+};
 
-descriptionsOfPictures.forEach(({url, description, likes, comments}) => {
-  const pictureElement = similarPictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__img.').src = url;
-  pictureElement.querySelector('.picture__img.').alt = description;
-  pictureElement.querySelector('.picture__comments').textContent = comments.length;
-  pictureElement.querySelector('.picture__likes.').textContent = likes;
-  picturesListFragment.appendChild(pictureElement);
-});
+const renederPictures = (pictures) => {
+  const picturesListFragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    const pictureElement = createPicture(picture);
+    picturesListFragment.append(pictureElement);
+  });
 
-pictureListElement.appendChild(picturesListFragment);
+  picturesList.append(picturesListFragment);
+};
+
+export {renederPictures};
 
