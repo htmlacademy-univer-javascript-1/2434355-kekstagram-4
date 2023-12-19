4738ec9184bb85f1a99065c7c835f82edea35f63
Dariaeg <dariaeg04@gmail.com>
2023-12-19 14:53:00 +0500

    pictures

--- js/pictures.js
+++ js/pictures.js
@@ -1,15 +1,15 @@
-import {createDescription} from './data.js';
+import {photoDescriptions} from './data.js';
 
 const pictureListElement = document.querySelector('.pictures');
 const similarPictureTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
-const descriptionsOfPictures = createDescription();
+const descriptionsOfPictures = photoDescriptions();
 
 const picturesListFragment = document.createDocumentFragment();
 
-descriptionsOfPictures.array.forEach(({url, description, likes, comments}) => {
+descriptionsOfPictures.forEach(({url, description, likes, comments}) => {
   const pictureElement = similarPictureTemplate.cloneNode(true);
   pictureElement.querySelector('.picture__img.').src = url;
   pictureElement.querySelector('.picture__img.').alt = description;
