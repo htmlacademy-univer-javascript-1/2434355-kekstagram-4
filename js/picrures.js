b97a141bb16542361b93346dee8e7e7bf7ff7d70
Dariaeg <dariaeg04@gmail.com>
2023-12-19 10:21:21 +0500

    правка

--- js/picrures.js
+++ js/picrures.js
@@ -5,16 +5,16 @@ const similarPictureTemplate = document.querySelector('#picture')
   .content
   .querySelector('.picture');
 
-const descriptions = createDescription();
+const descriptionsOfPictures = createDescription();
 
 const picturesListFragment = document.createDocumentFragment();
 
-descriptions.forEach(({url, description, likes, comments}) => {
+descriptionsOfPictures.array.forEach(({url, description, likes, comments}) => {
   const pictureElement = similarPictureTemplate.cloneNode(true);
-  pictureElement.src = url;
-  pictureElement.alt = description;
+  pictureElement.querySelector('.picture__img.').src = url;
+  pictureElement.querySelector('.picture__img.').alt = description;
   pictureElement.querySelector('.picture__likes.').textContent = likes;
-  pictureElement.querySelector('.picture__comments').textContent = comments;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
   picturesListFragment.appendChild(pictureElement);
 });
 
