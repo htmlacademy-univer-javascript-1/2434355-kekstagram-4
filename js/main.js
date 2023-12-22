2d27282cf60dd428c16ed0d61e8dc077b592ac7c
Dariaeg <dariaeg04@gmail.com>
2023-12-22 22:15:27 +0500

    main

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 import './form.js';
 import {photoDescriptions} from './data.js';
 import {renderPictures} from './pictures.js';
+import {getPhotos} from './api.js';
 
-renderPictures(photoDescriptions());
+getPhotos(renderPictures(photoDescriptions()));
